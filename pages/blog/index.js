202602ada3a60677bb7873de86bb5c1418452d26
ekremgunden <eko1128244@gmail.com>
2021-06-30 01:04:15 +0300

    little change fix3

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -5,7 +5,7 @@ export default function Index(data) {
     <div className="site-container mt-6 flex flex-wrap justify-start items-start">
       {blogdata.map((item) =>{
         return(
-            <div className="bg-red-200 w-80 h-64 rounded-md px-6 py-4 my-5 mx-6 shadow-lg flex flex-col justify-between hover:bg-red-300 duration-300">
+            <div key={item.id} className="bg-red-200 w-80 h-64 rounded-md px-6 py-4 my-5 mx-6 shadow-lg flex flex-col justify-between hover:bg-red-300 duration-300">
               <div>
                 <h2 className="font-semibold text-xl">{item.title}</h2>
                 <p>{item.description}</p>
@@ -29,5 +29,7 @@ export async function getStaticProps() {
     props: {
       data
     },
+    revalidate: 60,
+    
   }
 }
